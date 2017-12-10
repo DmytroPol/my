f993d4b288bf480f912ab8cadf17dff8337808b8
DmytroPol <33494689+DmytroPol@users.noreply.github.com>
2017-12-10 20:29:30 +0200

    HomePro1.1

--- Pro/Main.java
+++ Pro/Main.java
@@ -1,59 +1,38 @@
 package com.gmail.dmytropolishchuk2;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.reflect.InvocationTargetException;
+import java.lang.annotation.Target;
 import java.lang.reflect.Method;
 
-@Retention(value = RetentionPolicy.RUNTIME)
-@interface Saver {} // как сохранить
-
-@Retention(value = RetentionPolicy.RUNTIME)
-@interface SaveTo { // куда сохранить
-    String filename();
+@Retention(value= RetentionPolicy.RUNTIME)// доступна на этапе выполнения
+@Target(value= ElementType.METHOD) // цель аннотации(метод)
+@interface Test {// параметры аннотации
+    int a();
+    int b();
 }
 
-@SaveTo(filename = "text.txt")
-class TextContainer { // что сохранить
-     String text="Hello world!";
-
-    @Saver
-    public void saver(String filename) throws IOException {
-        FileWriter fw = new FileWriter(filename);// записать в файл
-        try{
-            fw.write(text); // что именно
-        }
-        finally
-        {
-            fw.close(); // закрыть
-        }
-    }
-}
-
-class SaveText {
-    public static void saveText(TextContainer txt) { // список параметров
-        Class<?> cls = txt.getClass(); // с какого класса
-        if (cls.isAnnotationPresent(SaveTo.class)) { // проверка на наличие аннотации
-            for (Method meth : cls.getMethods()) { // получить методы
-                if (meth.isAnnotationPresent(Saver.class)) {
-                    try {
-                        meth.invoke(txt, cls.getAnnotation(SaveTo.class).filename());
-                        System.out.printf("Text successfully saved to file by method %s()", meth.getName());
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        System.out.println(e);
-                    }
-                }
-            }
-        }
-    }
+class Sum {
+    @Test(a = 3, b = 5)
+    public static void test(int a, int b) {
+        System.out.println("a + b = " + (a + b));
+    }// метод
 }
 
 public class Main {
-
     public static void main(String[] args) {
-        TextContainer txtcont = new TextContainer();
-        SaveText.saveText(txtcont);
+
+        Class<?> cls = Sum.class;// с какого класса
+        try{
+            Method method = cls.getMethod("test", int.class, int.class);// получить ссылку на метод
+            if (method.isAnnotationPresent(Test.class)) {// проверка обьеккта на наличия аннотации truе или false
+
+                Test my = method.getAnnotation(Test.class);// возвращает ссылку на аннотацию
+                method.invoke(cls, my.a(), my.b());// вызов метода с передачей ему параметров аннотации
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
 }
