4659efe43802688572e74d01dcd2c9ab45aa6ee0
DmytroPol <33494689+DmytroPol@users.noreply.github.com>
2018-01-08 18:09:26 +0200

    GetListServlet

--- Pro3.1/GetListServlet.java
+++ Pro3.1/GetListServlet.java
@@ -14,9 +14,9 @@ public class GetListServlet extends HttpServlet {
 
     @Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		String fromStr = req.getParameter("from");
-        String userName = req.getParameter("userName"); //Serg
-        String password = req.getParameter("password"); //Serg
+	String fromStr = req.getParameter("from");
+        String userName = req.getParameter("userName"); 
+        String password = req.getParameter("password"); 
 		int from = 0;
 		try {
 			from = Integer.parseInt(fromStr);
@@ -24,20 +24,6 @@ public class GetListServlet extends HttpServlet {
 			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
 		}
-//serg
-//        int res = 200;
-//        UserList userList = UserList.getInstance();
-//        User user = userList.getUser(userName);
-//        if (user == null) {
-//            System.out.println("USER " + userName + " DOES NOT EXISTS");
-//            return;
-//        } else {
-//            if (!user.getPassword().equals(password)) {
-//                System.out.println("USER " + userName + " - BAD PASSWORD");
-//                return;
-//            }
-//        }
-
 		String json = msgList.toJSON(from);
 		if (json != null) {
 			OutputStream os = resp.getOutputStream();
